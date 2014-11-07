5a0a18d5cb558d66974bc064593d180a7def5089
Jason Young <jason@ytechie.com>
2014-11-07 12:31:26 -0600

    Added a basic alarm type

---
+++ Framework/Alarms/Alarm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Manufacturing.Framework.Alarms
+{
+    public class Alarm
+    {
+        public DateTime Timestamp { get; set; }
+        public string Message { get; set; }
+        public bool Acknowledged { get; set; }
+    }
+}
